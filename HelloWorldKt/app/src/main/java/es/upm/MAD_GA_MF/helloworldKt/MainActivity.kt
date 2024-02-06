8d81df4e70b543a763fd290b6505c678683258dc
Gabriel Amis <gabriel.amis@epita.fr>
2024-02-06 16:53:12 +0100

    Project

---
+++ HelloWorldKt/app/src/main/java/es/upm/MAD_GA_MF/helloworldKt/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.upm.MAD_GA_MF.helloworldKt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
