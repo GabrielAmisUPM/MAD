bce2519c0685d2a3544bd65d41a9f4307e50b3b9
mai maison <maiwenn.foucher@epita.fr>
2024-02-21 00:08:53 +0100

    w2

--- HelloWorldKt/app/src/main/java/es/upm/MAD_GA_MF/helloworldKt/SecondActivity.kt
+++ HelloWorldKt/app/src/main/java/es/upm/MAD_GA_MF/helloworldKt/SecondActivity.kt
@@ -1,24 +1,40 @@
 package es.upm.MAD_GA_MF.helloworldKt
 
+import android.content.ContentValues.TAG
 import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
+    private val TAG = "btaSecondActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val buttonPrev: Button = findViewById(R.id.S_mainButton)
-        buttonPrev.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
 
-        val buttonNext: Button = findViewById(R.id.S_thirdButton)
+        Log.d(TAG, "onCreate: The activity is being created.");
+
+        val bundle = intent.getBundleExtra("locationBundle")
+        val location: Location? = bundle?.getParcelable("location")
+
+        if (location != null) {
+            Log.i(TAG, "onCreate: Location["+location.altitude+"]["+location.latitude+"]["+location.longitude+"][")
+        };
+
+        val buttonNext: Button = findViewById(R.id.SecondButton)
         buttonNext.setOnClickListener {
             val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
+
+        val buttonPrevious: Button = findViewById(R.id.ReturnButton)
+        buttonPrevious.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- HelloWorldKt/app/src/main/java/es/upm/MAD_GA_MF/helloworldKt/ThirdActivity.kt
+++ HelloWorldKt/app/src/main/java/es/upm/MAD_GA_MF/helloworldKt/ThirdActivity.kt
@@ -1,16 +1,30 @@
 package es.upm.MAD_GA_MF.helloworldKt
 
+import android.content.ContentValues
 import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
-        val buttonPrev: Button = findViewById(R.id.T_secondButton)
-        buttonPrev.setOnClickListener {
+
+        Log.d(ContentValues.TAG, "onCreate: The activity is being created.");
+
+        val bundle = intent.getBundleExtra("locationBundle")
+        val location: Location? = bundle?.getParcelable("location")
+
+        if (location != null) {
+            Log.i(ContentValues.TAG, "onCreate: Location["+location.altitude+"]["+location.latitude+"]["+location.longitude+"][")
+        };
+
+
+        val buttonPrevious: Button = findViewById(R.id.ThirdButton)
+        buttonPrevious.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
